a431b718b22a65c9884134f45500fcddd17cf433
Esaam Shaukat <essam.shaukat94@gmail.com>
2026-07-04 17:47:32 +0300

    Rename PWA install app

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='wafrah-public-v0.1.9';
+const CACHE='wafrah-public-v0.1.10';
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'])));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
